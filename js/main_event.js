3fd78b580b5ae45c6aba101b5d939a1fe71433a3
wsseong <woo_seock@naver.com>
2021-12-09 20:04:35 +0900

    slide 이벤트

---
+++ js/main_event.js
@@ -0,0 +1,9 @@
+const swiper_slider = new Swiper('.swiper-container' , {
+    speed: 500,
+    debugger: true,
+    loop: true,
+    mousewheel: false,
+    autoplay: { 
+        delay: 3000
+    }  
+});
